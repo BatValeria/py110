d38252227fb6ba96321a45cec33c0e6e8cb13bb5
Valeria <alle20042003@gmail.com>
2022-11-26 16:19:58 +0300

    task5_2

--- Занятие1/Лабораторные_задания/task5_2/main.py
+++ Занятие1/Лабораторные_задания/task5_2/main.py
@@ -1,7 +1,7 @@
 def task() -> list:
     temp_tuple = (0, 36.6, 100)
 
-    return ...  # TODO  вернуть список температур по Фаренгейту
+    return [(index * 9/5)+ 32 for index in temp_tuple]  # TODO  вернуть список температур по Фаренгейту
 
 
 if __name__ == "__main__":
